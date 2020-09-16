b659e347ee9e835a634c17a356baec3764f1dcf6
Christofer Karankevits <chris.karankevits@gmail.com>
2020-09-16 10:04:19 +0300

    Добавлены Комментарии и подсказки

--- src/sptvr19game/App.java
+++ src/sptvr19game/App.java
@@ -5,6 +5,9 @@
  */
 package sptvr19game;
 
+import java.util.Scanner;
+import java.util.Random;
+
 /**
  *
  * @author user
@@ -12,6 +15,35 @@ package sptvr19game;
 class App {
     public void run() {
         System.out.println("Угадай задуманное число!");
-}
+        System.out.println("----------------------------");
+        Random random = new Random();
+        int myNumber = random.nextInt(5-0+1)+0;
+        System.out.println("Задумано число от 0 до 5. Угадай!");
+        Scanner scanner = new Scanner(System.in);
+        int attempt = 1;
+        do{
+            
+            int gameNumber = scanner.nextInt();
+            if (myNumber == gameNumber) {
+                System.out.println("Ты выиграл!");
+                break;
+            }else{
+                if(attempt < 3) {
+                    System.out.println("Не угадал попробуй ещё!");
+                    if(myNumber > gameNumber) {
+                    System.out.println("Задуманное число больше!");
+                    } else{
+                        System.out.println("Задуманное число меньше!");
+                    }
+                    
+                } else {
+                     System.out.println("ТЫ проиграл! Задумано число: "+myNumber);
+                     break;
+                }
+            }
+            attempt++;
+        }while(true);
+        
+    }
     
 }
